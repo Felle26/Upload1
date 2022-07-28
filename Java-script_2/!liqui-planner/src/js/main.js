3f14f2f240383b1579437470bd619f5a15780eb5
Felle26 <sebastianfelsberg@web.de>
2022-07-28 14:07:02 +0200

    modified:   Java-script_2/!liqui-planner/src/js/main.js 	modified:   Java-script_2/!liqui-planner/src/js/main.js 	modified:   Java-script_2/Objekte/verschachtelte Objekte/main.js
    	"Java-script_2/Objekte/Eigenschaften \303\244nder hinzufuegen oder entfernen/"
    	Java-script_2/Objekte/Methoden/
    	Java-script_2/Objekte/Objekte Vergleichen/
    	Java-script_2/Objekte/Objekte als Konstanten/
    	"Java-script_2/Objekte/Objekte als R\303\274ckgabewerte/"
    	Java-script_2/Objekte/Objekte in Funktionen/
    	Java-script_2/Objekte/bracket notation/
    	Java-script_2/Objekte/dot notation/
    	Java-script_2/call-by-value vs call-by-refference/

--- Java-script_2/!liqui-planner/src/js/main.js
+++ Java-script_2/!liqui-planner/src/js/main.js
@@ -1,56 +1,55 @@
 "use strict";
 
-let Einnahme = 0;
-let ausgaben = 0;
-let bilanz = 0;
-const euro = 100;
-
-let titel,
-    typ,
-    betrag,
-    datum;
-
-const eintrag_erfassen = function () {
-    titel = prompt("was war es?");
-    typ = prompt("e oder a");
-    betrag = parseInt(prompt("Der Betrag in cent:"));
-    datum = prompt("Wann war es , JJJJ-MM-TT");
-}
-
-const eintrag_ausgeben = function (titel, typ, betrag, datum) {
-        return (`Titel: ${titel}
-    Typ: ${typ}
-    Betrag: ${betrag / euro} €
-    Datum ${datum}`)
-};
-
-const eintrag_mit_gesamtbilanz_verrechnen = function (typ, betrag) {
-    if (typ === "e") {
-        Einnahme = Einnahme + betrag;
-        bilanz = bilanz + betrag;
-    } else if (typ === "a") {
-        ausgaben = ausgaben + betrag
-        bilanz = bilanz - betrag;
-    } else {
-        console.log(`${typ} ist nicht definiert`)
+let Haushaltsbuch = {
+    euro: 100,
+    gesamt_bilanz:{
+        bilanz: 0,
+        einnahmen: 0,
+        ausgaben:0
+    },
+    neuer_eintrag: {
+        titel:"lohn",
+        typ: "e",
+        betrag: 0,
+        datum: 0
+    },
+    eintrag_erfassen() {
+        this.neuer_eintrag.titel = prompt("was war es?");
+        this.neuer_eintrag.typ = prompt("e oder a");
+        this.neuer_eintrag.betrag = parseInt(prompt("Der Betrag in cent:"));
+        this.neuer_eintrag.datum = prompt("Wann war es , JJJJ-MM-TT");
+    },
+    eintrag_ausgeben() {
+        return (`Titel: ${this.neuer_eintrag.titel}
+    Typ: ${this.neuer_eintrag.typ}
+    Betrag: ${this.neuer_eintrag.betrag / this.euro} €
+    Datum ${this.neuer_eintrag.datum}`)
+    },
+    eintrag_mit_gesamtbilanz_verrechnen() {
+        if (this.neuer_eintrag.typ === "e") {
+            this.gesamt_bilanz.einnahmen += this.neuer_eintrag.betrag;
+            this.gesamt_bilanz.bilanz += this.neuer_eintrag.betrag;
+        } else if (this.neuer_eintrag.typ === "a") {
+            this.gesamt_bilanz.ausgaben += this.neuer_eintrag.betrag;
+            this.gesamt_bilanz.bilanz -= this.neuer_eintrag.betrag;
+        } else {
+            console.log(`${this.neuer_eintrag.typ} ist nicht definiert`)
+        }
+    },
+    gesamt_bilanz_ausgeben() {
+        console.log(`Einnahmen: ${this.gesamt_bilanz.einnahmen / this.euro}€
+    Ausgaben: ${this.gesamt_bilanz.ausgaben / this.euro}€
+    Bilanz: ${this.gesamt_bilanz.bilanz / this.euro}€
+    Deine Bilanz ist positiv: ${this.gesamt_bilanz.bilanz >= 0}`
+        );
+    },
+    eintrag_hinzufuegen() {
+        this.eintrag_erfassen();
+        console.log(this.eintrag_ausgeben(this.eintrag_erfassen.titel, this.eintrag_erfassen.typ, this.eintrag_erfassen.betrag, this.eintrag_erfassen.datum));
+        this.eintrag_mit_gesamtbilanz_verrechnen(this.eintrag_erfassen.typ, this.eintrag_erfassen.betrag);
+        this.gesamt_bilanz_ausgeben(this.gesamt_bilanz.einnahmen, this.gesamt_bilanz.ausgaben, this.gesamt_bilanz.bilanz);
     }
 };
 
-const gesamt_bilanz_ausgeben = function (Einnahme, ausgaben, bilanz) {
-        console.log(`Einnahmen: ${Einnahme / euro}€
-    Ausgaben: ${ausgaben / euro}€
-    Bilanz: ${bilanz / euro}€
-    Deine Bilanz ist positiv: ${bilanz >= 0}`
-    );
-};
-
-const eintrag_hinzufuegen = function(){
-    eintrag_erfassen();
-    console.log(eintrag_ausgeben(titel, typ, betrag, datum));
-    eintrag_mit_gesamtbilanz_verrechnen(typ, betrag);
-    gesamt_bilanz_ausgeben(Einnahme, ausgaben, bilanz);
-};
-
-eintrag_hinzufuegen();
-eintrag_hinzufuegen();
-eintrag_hinzufuegen();
+Haushaltsbuch.eintrag_hinzufuegen()
+Haushaltsbuch.eintrag_hinzufuegen()
