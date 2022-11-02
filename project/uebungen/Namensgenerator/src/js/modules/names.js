14cbcdff92cbf0a633779b4a77793da03b062789
Felle26 <sebastianfelsberg@web.de>
2022-11-02 20:06:18 +0100

    modified:   project/uebungen/Namensgenerator/index.html 	modified:   project/uebungen/Namensgenerator/src/js/main.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/Elfen_frauen_namen.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/Elfen_maenner_namen.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/frauen_namen.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/maenner_namen.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/nachnamen.js 	new file:   project/uebungen/Namensgenerator/src/js/modules/names.js 	modified:   project/uebungen/Taxameter/index.html 	modified:   python.code-workspace

---
+++ project/uebungen/Namensgenerator/src/js/modules/names.js
@@ -0,0 +1,4 @@
+export {frauen_elfen} from "./Elfen_frauen_namen.js";
+export {frauen_namen} from "./frauen_namen.js";
+export {maenner_elfen_namen} from "./Elfen_maenner_namen.js";
+export {maenner_namen} from "./maenner_namen.js"
